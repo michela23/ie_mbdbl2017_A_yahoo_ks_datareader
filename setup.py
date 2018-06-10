ebd2a0753dfba6c475a3de8e03c2344e46435ddf
Michel Abi Aad <michelabiaad@gmail.com>
2018-06-10 22:45:24 +0300

    1st commit

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='ie_mbdbl2017_A_yahoo_ks_datareader',
-      version='0.2',  # Development release
-      description='Python Packaging Starting Kit - Simple Structure to creater your own package - for training purposes!',
+      version='0.3',  # Development release
+      description='Python Packaging Starting Kit - Simple Structure to create your own package - for training purposes!',
+	  long_description='Python Packaging Starting Kit - Simple Structure to create your own package - for training purposes!',
       url='https://github.com/michela23/ie_mbdbl2017_A_yahoo_ks_datareader',
-      author='Group A IE GMBD 2017',
-      author_email='michel.aa@faculty.ie.edu',
+      author='IE GMBD 2017 Group A',
+      author_email='michel.aa@student.ie.edu',
       license='MIT',
           packages=['ie_mbdbl2017_A_yahoo_ks_datareader'],
       zip_safe=False)
