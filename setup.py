11b29a9477411e50658d95d210be96fdcd23bf68
Michel Abi Aad <michelabiaad@gmail.com>
2018-06-10 23:18:38 +0300

    1st commit

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='ie_mbdbl2017_A_yahoo_ks_datareader',
-      version='0.4',  # Development release
-      description='Python Packaging Starting Kit - Simple Structure to create your own package - for training purposes!',
-	  long_description='Python Packaging Starting Kit - Simple Structure to create your own package - for training purposes!',
+      version='0.5',  # Development release
+      description='Python package to retrieve the Price/Sales for technology stocks starting 06/03/2018 as part of Risk & Fraud course IE GMBD 2017',
+	  long_description='Python package to retrieve the Price/Sales for technology stocks starting 06/03/2018 as part of Risk & Fraud course IE GMBD 2017',
       url='https://github.com/michela23/ie_mbdbl2017_A_yahoo_ks_datareader',
       author='IE GMBD 2017 Group A',
       author_email='michel.aa@student.ie.edu',
